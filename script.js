b48a97676c5c51e8fd4e09b87ffedf01490a96e6
sahilsalam007 <salamsahil474@gmail.com>
2024-10-17 17:25:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// function sendemail() {
+//     var params = {
+//         to_name : document.getElementById("full-name").value,
+//         email_id : document.getElementById("email-id").value,
+//         subject : document.getElementById("subject").value,
+//         Message : document.getElementById("message").value
+//     }
+//     console.log(params)
+//     emailjs.send("service_u4o8fbg" , "template_d44ryme" , params).then(function(res){
+//         alert("success!" + res.status);
+//     })
+// }
